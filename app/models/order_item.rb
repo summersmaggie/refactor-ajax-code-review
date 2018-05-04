ac93d9f184c52d2d75535108394f6129b6fdf87e
Maggie Summers <summers.maggie@gmail.com>
2018-05-04 11:03:05 -0700

    add product validation and pass test specs

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,4 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
   validates :quantity, numericality: { greater_than: 0 }
-
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_many :order_items
+  validates :name, :presence => true
+  validates :description, :presence => true
+  validates :price, :presence => true
+  validates :image, :presence => true
 end
