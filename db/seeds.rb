fbff5ac0968f3fc248c5512b7af12675bc0d8090
Maggie Summers <summers.maggie@gmail.com>
2018-05-04 09:44:32 -0700

    add AJAX functionality to allow user to click on product to reveal image link to product

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ class Seed
 
   def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word, price: rand(10...100), image: Faker::Internet.url, description: Faker::Lorem.sentence(5, false, 0).chop)
     end
   end
 end
