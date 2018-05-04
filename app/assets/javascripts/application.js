d60d19ad742133c71990d38d381a38490e71c0d2
Maggie Summers <summers.maggie@gmail.com>
2018-05-04 09:36:04 -0700

    add AJAX functionality to allow user to click on product to reveal description of product on product page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require rails-ujs
+//= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require materialize-sprockets
+//= require jquery
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_04_12_212644) do
+ActiveRecord::Schema.define(version: 2018_05_04_161949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2017_04_12_212644) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
